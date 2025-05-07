ab1834329b68e11e1ffbf4cb865b4bf3772fa94e
Sohee Kim <ttohee1275@gmail.com>
2025-05-07 23:22:05 +0900

    chore: initial setting

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./index.css";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Landingpage from "./pages/Landingpage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landingpage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Landingpage.tsx
@@ -0,0 +1,5 @@
+const Landingpage = () => {
+  return <div>hello</div>;
+};
+
+export default Landingpage;
